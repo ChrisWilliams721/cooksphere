dbcde726b0283a3030ec70fdf4bad28eb455f3db
Chris Williams <cchriswilliams721@gmail.com>
2024-12-07 16:58:28 -0700

    Firebase Auth

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,15 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
-import MobileMenu from "./MobileMenu";
-import {
-  ClerkLoaded,
-  ClerkLoading,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+
 
 function Navbar() {
   return (
@@ -48,35 +40,7 @@ function Navbar() {
       </div>
       {/* RIGHT */}
       <div className="w-[30%] flex items-center justify-center gap-2 text-sm  ">
-        <ClerkLoading>
-          <div
-            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
-            role="status"
-          >
-            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-              Loading...
-            </span>
-          </div>
-        </ClerkLoading>
-        <ClerkLoaded >
-          <SignedIn>
-            <div className="cursor-pointer"></div>
-            <UserButton />
-          </SignedIn>
-          <SignedOut>
-            <div className="flex items-center justify-center gap-2 text-sm ">
-              <Image
-                src="/login.png"
-                alt="logo"
-                width={16}
-                height={16}
-                className="w-4 h-4"
-              />
-              <Link href="/sign-in">Login/Signup</Link>
-            </div>
-          </SignedOut>
-        </ClerkLoaded>
-        <MobileMenu />
+
       </div>
     </div>
   );
